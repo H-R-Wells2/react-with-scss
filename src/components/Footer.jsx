76331881a44bc1293a8e051a2dbdf06bd3eb204c
h-r-wells2 <hrwells10246@gmail.com>
2023-06-21 18:21:30 +0530

    react with scss

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>HRWells</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://t.me/h_r_wells" target={"blank"}>
+            Telegram
+          </a>
+          <a href="https://instagram.com/shubhhammmmm" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/h-r-wells2" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
